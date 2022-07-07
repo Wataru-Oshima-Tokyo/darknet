09fe1d4d57e6a2160d7cc508df490db1b9f920ea
Wataru-Oshima-Tokyo <wataruoshima@my.smccd.edu>
2022-07-07 11:34:50 +0900

    added outlet

---
+++ outlet/outlet/convert.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:53981daba0526deadae9903394b5aef70eca56655e67a466e37c53026b20d151
+size 2816
---
+++ outlet/outlet/resize.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6a5167bed02ebf6a3a4622ef7a06b82bd05ae76d146261811d039d92ee65b3c9
+size 615
